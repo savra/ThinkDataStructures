a47e0e2dd77b86d04173a51bdcc2857e996bfeb7
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-19 15:49:29 +0300

    solution for fifth exercise

--- code/src/com/allendowney/thinkdast/WikiPhilosophy.java
+++ code/src/com/allendowney/thinkdast/WikiPhilosophy.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
@@ -12,15 +14,17 @@ public class WikiPhilosophy {
     final static List<String> visited = new ArrayList<String>();
     final static WikiFetcher wf = new WikiFetcher();
 
+    Node bracket;
+
     /**
      * Tests a conjecture about Wikipedia and Philosophy.
-     *
+     * <p>
      * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
-     *
+     * <p>
      * 1. Clicking on the first non-parenthesized, non-italicized link
      * 2. Ignoring external links, links to the current page, or red links
      * 3. Stopping when reaching "Philosophy", a page with no links or a page
-     *    that does not exist, or when a loop occurs
+     * that does not exist, or when a loop occurs
      *
      * @param args
      * @throws IOException
@@ -40,6 +44,64 @@ public class WikiPhilosophy {
      * @throws IOException
      */
     public static void testConjecture(String destination, String source, int limit) throws IOException {
-        // TODO: FILL THIS IN!
+
+        Elements elements = wf.fetchWikipedia(source);
+
+        for (Element node : elements) {
+            if (recursiveDFS(node) != null) {
+                System.out.println("Philosophy found");
+            } else {
+                System.out.println("Philosophy not found");
+            }
+        }
+
+        visited.forEach(System.out::println);
+    }
+
+    private static Node recursiveDFS(Node root) {
+        if (!(root instanceof TextNode)) {
+            Element element = (Element) root;
+            if ("a".equals(element.tag().getName())) {
+                if (element.attributes().get("href").endsWith("/wiki/Philosophy")) {
+                    visited.add(element.attributes().get("href"));
+                    return element;
+                } else {
+                    visited.add(element.attributes().get("href"));
+                }
+            }
+        }
+
+        for (Node node : root.childNodes()) {
+            Node targetNode = recursiveDFS(node);
+
+            if (targetNode != null) {
+                if (targetNode.parent().getClass() == Element.class) {
+                    if (targetNode.childNodeSize() == 1
+                            && targetNode.childNode(0).getClass() == TextNode.class
+                            && ((TextNode) targetNode.childNode(0)).text().startsWith("P")) {
+                        if (!((Element) targetNode.parent()).tag().getName().equals("i")) {
+
+                            int i = targetNode.siblingIndex();
+
+                            if (i == 0 || i == targetNode.parent().childNodeSize() - 1
+                                    || (targetNode.parent().childNode(i - 1).getClass() == TextNode.class
+                                    && targetNode.parent().childNode(i + 1).getClass() == TextNode.class)) {
+                                TextNode prev = (TextNode) targetNode.parent().childNode(i - 1);
+                                TextNode next = (TextNode) targetNode.parent().childNode(i + 1);
+
+                                if (prev.text().charAt(prev.text().length() - 1) == '('
+                                        && next.text().charAt(0) == ')') {
+                                    return targetNode;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return targetNode;
+        }
+
+        return null;
     }
 }
